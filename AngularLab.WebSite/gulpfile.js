7db5bab7d5de2af227d2efc1c18550d168fb1694
jitto <jitto@tiempodevelopment.com>
2016-07-25 16:04:19 -0700

    gulp changes

--- AngularLab.WebSite/gulpfile.js
+++ AngularLab.WebSite/gulpfile.js
@@ -5,9 +5,14 @@
     var browserify = require("browserify");
     var watchify = require("watchify");
     var browserSync = require('browser-sync').create();
+    var argv = require('yargs').argv;
+    var inquirer = require('inquirer');
+    var fs = require('fs');
+    var path = require('path');
+    var beautify = require('js-beautify').js_beautify;
     var env = {
         debug: true,
-        url : "http://localhost:57449/"
+        url: "http://localhost:57449/"
     };
 
     gulp.task("bundler", function () {
@@ -22,9 +27,9 @@
 
         function build(file) {
             return bundler.bundle()
-                          .on('error', gutil.log.bind(gutil, "browserify error"))
-                          .pipe(source('main.js'))
-                          .pipe(gulp.dest('./Scripts/app/'));
+                .on('error', gutil.log.bind(gutil, "browserify error"))
+                .pipe(source('main.js'))
+                .pipe(gulp.dest('./Scripts/app/'));
         };
 
         build();
@@ -42,5 +47,50 @@
         gulp.watch("Scripts/app/main.js").on('change', browserSync.reload);
     });
 
-    gulp.task('default', ['bundler','serve']);
+    gulp.task("controller", function () {
+
+        var root = "Scripts/app/";
+
+        var questions = [{
+            type: 'input',
+            name: 'controller_name',
+            message: 'What\'s your controller name'
+        }, {
+            type: 'input',
+            name: 'route',
+            message: 'What\'s the route'
+        }];
+
+        inquirer.prompt(questions).then(function (answers) {
+            var newControllerPath = root + "/" + answers.controller_name;
+            fs.mkdirSync(newControllerPath);
+            fs.mkdirSync(newControllerPath + "/controllers");
+            fs.mkdirSync(newControllerPath + "/views");
+
+            var controllerTemplate = beautify(getControllerTemplate(answers.controller_name),{ indent_size: 4 });
+
+            fs.writeFile(newControllerPath + "/controllers/" + answers.controller_name + "-controller.js", controllerTemplate, function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+
+            fs.writeFile(newControllerPath + "/views/" + answers.controller_name + "-view.html", "", function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+            });
+        });
+    });
+
+    function getControllerTemplate(controllerName) {
+        var sb = " var " + controllerName + " = {" +
+                    "        imports: ['$scope']," +
+                    "        body: function ($scope) {" +
+                    "        }" +
+                    "    };";
+        return sb;
+    }
+
+    gulp.task('default', ['bundler', 'serve']);
 }());
