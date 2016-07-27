bb0421b712be5d93c4a8f380e3083e2bfd2b0cac
pablo <juanitto_40@hotmail.com>
2016-07-26 20:20:42 -0700

    create services

--- AngularLab.WebSite/Scripts/app/shared/index.js
+++ AngularLab.WebSite/Scripts/app/shared/index.js
@@ -1,5 +1,4 @@
-﻿
-var app = require("angular").module('angularLabs');
+﻿var app = require("angular").module('angularLabs');
 var factory = require("./../component-factory");
 app.factory('commonService', factory.createComponent(require('./services/common-service')));
 app.directive('menu', factory.createComponent(require('./directives/menu/menu')));
--- AngularLab.WebSite/Scripts/app/shared/services/common-service.js
+++ AngularLab.WebSite/Scripts/app/shared/services/common-service.js
@@ -1,27 +1,22 @@
-﻿(function () {
+﻿module.exports = {
+    imports: ['$http'],
+    body: function ($http) {
 
-    var CommonService = {
-        imports: ['$http'],
-        body: function ($http) {
-
-            function doPost(p) {
-                return $http.post(p.url, p.data).then(function (response) {
-                    return response.data;
-                });
-            }
-
-            function doGet(p) {
-                return $http.get(p.url).then(function (response) {
-                    return response.data;
-                });;
-            }
-
-            return {
-                doPost: doPost,
-                doGet: doGet
-            };
+        function doPost(p) {
+            return $http.post(p.url, p.data).then(function (response) {
+                return response.data;
+            });
         }
-    };
 
-    module.exports = CommonService;
-}());
+        function doGet(p) {
+            return $http.get(p.url).then(function (response) {
+                return response.data;
+            });;
+        }
+
+        return {
+            doPost: doPost,
+            doGet: doGet
+        };
+    }
+};
