04d1a045df89f277c3d19225e4cbc1e4eb2a64ce
jitto <jitto@tiempodevelopment.com>
2016-07-27 14:36:56 -0700

    refactor menu layout

--- AngularLab.WebSite/Scripts/app/shared/index.js
+++ AngularLab.WebSite/Scripts/app/shared/index.js
@@ -1,4 +1,5 @@
-﻿var app = require("angular").module('angularLabs');
+﻿
+var app = require("angular").module('angularLabs');
 var factory = require("./../component-factory");
 app.factory('commonService', factory.createComponent(require('./services/common-service')));
 app.directive('menu', factory.createComponent(require('./directives/menu/menu')));
